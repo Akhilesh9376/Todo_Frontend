ddb537062643c73e870d49674208753b3db933d7
iritikraj <ritik.raj@sarvm.ai>
2024-10-22 23:05:06 +0530

    Login and Signup Fixed

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,16 @@
-import Modal from "./Modal";
+import React, { useState } from "react";
+import Signup from "./Signup";
 
- function Login() {
-    return (
-      <>
-        {/*
+function Login() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleSingupOrLogin = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <>
+      {/*
           This example requires updating your template:
   
           ```
@@ -11,18 +18,16 @@ import Modal from "./Modal";
           <body class="h-full">
           ```
         */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-              className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img alt="Your Company"
+            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            {isLogin ? 'Login' : 'Sign Up'}
+          </h2>
+        </div>
+        {isLogin ? (
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6">
               <div>
@@ -30,17 +35,12 @@ import Modal from "./Modal";
                   Email address
                 </label>
                 <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
+                  <input id="email" name="email" type="email" required autoComplete="email"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-  
+
               <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
@@ -53,17 +53,12 @@ import Modal from "./Modal";
                   </div>
                 </div>
                 <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
+                  <input id="password" name="password" type="password" required autoComplete="current-password"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-  
+
               <div>
                 <button
                   type="submit"
@@ -73,18 +68,20 @@ import Modal from "./Modal";
                 </button>
               </div>
             </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <span to="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                
-              </span>
-            </p>
           </div>
-        </div>
-      </>
-    )
-  }
+        ) : (
+          <Signup />
+        )}
+        <p className="mt-10 text-center text-sm text-gray-500">
+          {isLogin ? "Don't have an account? " : "Already have an account? "}
+          <span onClick={handleSingupOrLogin}
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer">
+            {isLogin ? 'Register now' : 'Login'}
+          </span>
+        </p>
+      </div>
+    </>
+  )
+}
 
-
-export default Login ;
+export default Login;
